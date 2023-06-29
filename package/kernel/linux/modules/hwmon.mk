5b08f9eb3b3de4e03691042d65867a68a2fb9675
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2023-06-29 11:48:04 +0200

    kernel: modules: nct6775: adapt for kernel 6.1
    Adapt nct6775 hwmon kmods for building under kernel 6.1:
    * depend on nct6775-core for kernels >= 5.19
    * add nct6775-i2c to the package for kernel >=5.19
    
    Signed-off-by: Stefan Lippers-Hollmann <s.l-h@gmx.de>

--- package/kernel/linux/modules/hwmon.mk
+++ package/kernel/linux/modules/hwmon.mk
@@ -416,10 +416,16 @@ $(eval $(call KernelPackage,hwmon-mcp3021))
 
 define KernelPackage/hwmon-nct6775
   TITLE:=NCT6106D/6775F/6776F/6779D/6791D/6792D/6793D and compatibles monitoring support
-  KCONFIG:=CONFIG_SENSORS_NCT6775
-  FILES:=$(LINUX_DIR)/drivers/hwmon/nct6775.ko
-  AUTOLOAD:=$(call AutoProbe,nct6775)
-  $(call AddDepends/hwmon,@PCI_SUPPORT @TARGET_x86 +kmod-hwmon-vid)
+  KCONFIG:= \
+	CONFIG_SENSORS_NCT6775_CORE@ge5.19 \
+	CONFIG_SENSORS_NCT6775 \
+	CONFIG_SENSORS_NCT6775_I2C@ge5.19
+  FILES:= \
+	$(LINUX_DIR)/drivers/hwmon/nct6775-core.ko@ge5.19 \
+	$(LINUX_DIR)/drivers/hwmon/nct6775.ko \
+	$(LINUX_DIR)/drivers/hwmon/nct6775-i2c.ko@ge5.19
+  AUTOLOAD:=$(call AutoProbe,nct6775 nct6775-i2c@ge5.19)
+  $(call AddDepends/hwmon,@PCI_SUPPORT @TARGET_x86 +LINUX_6_1:kmod-i2c-core +LINUX_6_1:kmod-regmap-core +kmod-hwmon-vid)
 endef
 
 define KernelPackage/hwmon-nct6775/description
